b13dcbda1905a29d8d9d58161fcae5c715847843
Ken Mugrage <kmugrage@thoughtworks.com>
2015-02-18 10:19:50 -0600

    Fixed spacing for #1

--- site/index.php
+++ site/index.php
@@ -1,20 +1,16 @@
 <?php
-
-require_once("lib/hello_library.php");
-require_once("lib/math_library.php");
-
+    require_once("lib/hello_library.php");
+    require_once("lib/math_library.php");
 ?>
 
 <p>Hello There</p>
 <?php
-$helloThere = new HelloWorld();
-$helloThere->sayHi();
+    $helloThere = new HelloWorld();
+    $helloThere->sayHi();
 ?>
 
 <p>Basic Math</p>
 <?php
-$doMath = new BasicMath();
-
-print "2 + 2 = " . $doMath->addStuff(2, 2)
-
+    $doMath = new BasicMath();
+    print "2 + 2 = " . $doMath->addStuff(2, 2)
 ?> 
